26fbb1b84b97077665ad1efec15e84733450a3d1
eng-juliane <nascimento27ju@gmail.com>
2022-07-08 18:17:27 -0300

    commit inicial

---
+++ src/respostas/desafio2.ts
@@ -0,0 +1,41 @@
+enum Trabalho {
+    Atriz,
+    Engenheira,
+    Padeiro
+}
+
+type Humano = {
+    nome: string,
+    idade: number,
+    profissao: Trabalho
+}
+
+let pessoa1: Humano = {
+    nome: 'maria',
+    idade: 29,
+    profissao: Trabalho.Atriz
+};
+
+let pessoa2: Humano = {
+    nome: 'roberto',
+    idade: 19,
+    profissao: Trabalho.Padeiro
+};
+
+let pessoa3: Humano = {
+    nome: 'laura',
+    idade: 32,
+    profissao: Trabalho.Atriz
+};
+
+let pessoa4: Humano = {
+    nome: "carlos",
+    idade: 19,
+    profissao: Trabalho.Padeiro
+}
+
+let pessoa5: Humano = {
+    nome: "juliane",
+    idade: 24,
+    profissao: Trabalho.Engenheira
+}
